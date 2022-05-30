6f0b55cfb08ba11eabf58195d044a774fdb6b940
Sergei Ruban <sergei24ruban@gmail.com>
2022-05-30 16:36:04 +0300

    add welcome message to the game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
+import { userName } from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
+
+// eslint-disable-next-line no-unused-expressions
+userName;
---
+++ src/cli.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+import readlineSync from 'readline-sync';
+
+const userName = readlineSync.question('May I have your name? ');
+
+console.log(`Hello ${userName}!`);
+
+export default userName;
