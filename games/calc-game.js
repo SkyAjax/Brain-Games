a7e946111a4a978356e6ec0343859f198d109412
Sergei Ruban <sergei24ruban@gmail.com>
2022-07-07 17:25:53 +0300

    update calc-game

--- games/calc-game.js
+++ games/calc-game.js
@@ -1,19 +1,18 @@
 /* eslint-disable no-console */
-import { gameDescription, getRandomNum, oneRound, runRounds } from '../src/index.js';
+import { gameDescription, getRandomNum, oneRound } from '../src/index.js';
 
 // Wait for user's response.
 gameDescription('What is the result of the expression?');
-
-let correctAnswer;
 
 // Function that starts a round
 const startRound = () => {
   const firstNumber = getRandomNum();
   const secondNumber = getRandomNum();
 
-  // console.log(`${firstNumber} ${secondNumber}`);
+  console.log(`${firstNumber} ${secondNumber}`);
   const getRandomInt = Math.floor(Math.random() * 3);
   const typeOfMath = ['+', '-', '*'];
+  let correctAnswer;
 
   if (getRandomInt === 0) {
     correctAnswer = firstNumber + secondNumber;
@@ -26,4 +25,4 @@ const startRound = () => {
   oneRound(oneQuestion, correctAnswer);
 };
 
-runRounds(startRound());
+startRound();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
 import userName from './cli.js';
 
@@ -12,23 +13,19 @@ export const getRandomNum = () => Math.floor(Math.random() * 10);
 
 // Answer the question
 export const oneRound = (question, answer) => {
-  console.log(`Question: ${question}`);
-  const userAsnwer = readlineSync.question('Your answer: ');
-
-  if (Number(userAsnwer) === answer) {
-    console.log('Correct!');
-    return true;
-  }
-  console.log(`'${userAsnwer}' is wrong answer ;(. Correct answer was '${answer}'.
- Let's try again, ${userName}!`);
-  return false;
-};
-
-export const runRounds = (round) => {
   for (let i = 0; i < maxRounds; i += 1) {
-    if (round === false) {
+    console.log(i);
+    console.log(`Question: ${question}`);
+
+    const userAsnwer = readlineSync.question('Your answer: ');
+
+    if (Number(userAsnwer) === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAsnwer}' is wrong answer ;(. Correct answer was '${answer}'.
+ Let's try again, ${userName}!`);
       return;
     }
+    console.log(`Congratulations, ${userName}`);
   }
-  console.log(`Congratulations, ${userName}`);
 };
