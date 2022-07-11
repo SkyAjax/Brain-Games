253c7cf27e766fe0abbb60450da9ee1fc375382d
Sergei Ruban <sergei24ruban@gmail.com>
2022-07-12 01:54:08 +0300

    fix welcome message

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,6 @@ import readlineSync from 'readline-sync';
 // Wait for user's response.
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
-console.log(`Hi, ${userName}!`);
+console.log(`Hello, ${userName}!`);
 
 export default userName;
